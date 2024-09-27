86eca698144c8cd23eb140e67cf6c4538876ef07
Aziz <niftalizadeaziz@gmail.com>
2024-09-27 11:03:17 +0400

    commit 2 :)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.tree.TreeNode;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -305,7 +306,7 @@ public class Main {
 //    ===================================================================================================================
 
 
-//    private static void oxsarElementTapCollection(int[] array1, int[] array2) {
+    //    private static void oxsarElementTapCollection(int[] array1, int[] array2) {
 //        Set<Integer> yekun = new HashSet<>();
 //        Arrays.stream(array1).forEach(array1Element -> Arrays.stream(array2).forEach(array2Element -> {
 //            if (array1Element == array2Element) {
@@ -316,6 +317,167 @@ public class Main {
 //        System.out.println("--------------------------------------------\n");
 //    }
 //    ===================================================================================================================
+//    {1, 2, 3, 2, 5}
+//    private static void tekrarEded(int[] array) {
+//        int tekrarEded = -1;
+//        int counter = 0;
+//        int emeliyyat = 0;
+//        for (int i = counter; i < array.length; i++) {
+//            for (int j = counter + 1; j < array.length; j++) {
+//                if (array[i] == array[j]) {
+//                    tekrarEded = array[i];
+//                    break;
+//                }
+//                emeliyyat++;
+//            }
+//            counter++;
+//            if(tekrarEded!=-1)break;
+//        }
+//        if (tekrarEded >= 0) {
+//            System.out.println("tekrar edilen eded: " + tekrarEded);
+//        } else System.out.println("tekrar edilen eded yoxdur");
+//        System.out.println("emeliyyat sayi :" + emeliyyat);
+//        System.out.println("--------------------------------------------\n");
+//    }
+//    ===================================================================================================================
+//    private static void cutEdedlerinCemi(int[] array) {
+//        int toplam = 0;
+//        for (int i : array) {
+//            if (i % 2 == 0) {
+//                toplam += i;
+//            }
+//        }
+//        System.out.println("toplam = " + toplam);
+//        System.out.println("--------------------------------------------\n");
+//    }
+//    ===================================================================================================================
+//    {1, 2, 2, 3, 2, 6}
+//    private static void tekrarsizElementTopla(int[] array) {
+//        int toplam = 0;
+//        boolean tekrarsiz;
+//
+//        for (int i = 0; i < array.length; i++) {
+//            tekrarsiz = true;
+//            for (int j = 0; j < array.length; j++) {
+//                if (i != j && array[i] == array[j]) {
+//                    tekrarsiz = false;
+//                    break;
+//                }
+//            }
+//            if (tekrarsiz) {
+//                toplam += array[i];
+//            }
+//        }
+//        System.out.println("toplam = "+toplam);    }
+
+    //    {1, 4, 6, 2}, {2, 1, 4, 2, 5}
+//    private static void kesisenElementler(int[] array1, int[] array2) {
+//        Set<Integer> yekun = new HashSet<>();
+//        int counter = 0;
+//
+//
+//        for (int i = counter; i < array1.length; i++) {
+//            for (int j = i + 1; j < array2.length; j++) {
+//                if (array1[i] == array2[j]) {
+//                    yekun.add(array1[i]);
+//                    counter++;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        yekun.forEach(System.out::println);
+//    }
+//    private static void cemIndeksleriTap(int[] array, int cemdeLazimOlan) {
+//        int indeks1 = -1;
+//        int indeks2 = -1;
+//
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = i + 1; j < array.length; j++) {
+//                if (i != j && array[i] + array[j] == cemdeLazimOlan) {
+//                    indeks1 = i;
+//                    indeks2 = j;
+//                    break;
+//                }
+//            }
+//            if (indeks1 != -1 && indeks2 != -1) break;
+//        }
+//        if (indeks1 >= -1 && indeks2 >= -1) {
+//            System.out.println(cemdeLazimOlan + " ededi " + indeks1 + " ve " + indeks2 + " indekslerin elementini toplamaqla almaq olar");
+//        } else System.out.println(cemdeLazimOlan + " ededini almaq ucun elementler uygun deyil");
+//    }
+
+    //    {1,0,4,5,0,2,0}
+//    private static void sifirlariSagaYig(int[] array) {
+//
+//        int counter = 0;
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] != 0) {
+//                array[counter++] = array[i];
+//            }
+//        }
+//        for (int i = counter; i < array.length; i++) {
+//            array[i] = 0;
+//        }
+//        Arrays.stream(array).forEach(element -> System.out.print(element + ", "));
+//    }
+
+
+    //    public static void tekEdedleriqaytar(int[] array) {
+//        int tekEdedCounter = 0;
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0) {
+//                tekEdedCounter++;
+//            }
+//        }
+//
+//        int index = 0;
+//        int[] yekun = new int[tekEdedCounter];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 != 0) {
+//                yekun[index] = array[i];
+//                index++;
+//            }
+//        }
+//        Arrays.stream(yekun).sorted().forEach(System.out::println);
+//    }
+//
+//    private static void ikiArrayiSeteCevir(int[] array1, int[] array2) {
+//
+//        int[] birlesme = new int[array1.length + array2.length];
+//        int indeks = 0;
+//        for (int eded : array1) {
+//            birlesme[indeks++] = eded;
+//        }
+//
+//        for (int eded : array2) {
+//            birlesme[indeks++] = eded;
+//        }
+////        {1, 3, 1, 4, 6, 3, 5, 1, 7, 8}         {1,3,4,5,6,7,8}
+//        int counter = 0;
+//        int[] kecici = new int[birlesme.length];
+//        for (int i = 0; i < birlesme.length; i++) {
+//            boolean ferqlidir = true;
+//            for (int j = i + 1; j < birlesme.length; j++) {
+//                if (birlesme[j] == birlesme[i]) {
+//                    ferqlidir = false;
+//                    break;
+//                }
+//            }
+//            if (ferqlidir) {
+//                kecici[counter++] = birlesme[i];
+//            }
+//        }
+//        int[] yekun = new int[counter];
+//        for (int i = 0; i < yekun.length; i++) {
+//            yekun[i] = kecici[i];
+//        }
+//        Arrays.stream(yekun).sorted().forEach(element -> System.out.print(element + ", "));
+//    }
+
 
     public static void main(String[] args) {
 
@@ -332,13 +494,19 @@ public class Main {
 //        sagaUcbucaq();
 //        asagiUcbucaq();
 //        sadeMurekkebEdedTap(new Scanner(System.in).nextInt());
-        int[] arr1 = {0, 1, 2, 3, 4};
-        int[] arr2 = {2, 9, 11, 2, 3, 55, 4};
-//        oxsarElementTapCollection(arr1, arr2);
+//        oxsarElementTapCollection(new int[]{0, 1, 2, 3, 4}, new int[]{2, 9, 11, 2, 3, 55, 4});
+//        tekrarEded(new int[]{1, 2, 3, 9, 5, 8, 99, 88, 77, 66});
+//        cutEdedlerinCemi(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+//        tekrarsizElementTopla(new int[]{1, 2, 2, 3, 2, 6});
+//        kesisenElementler(new int[]{1, 4, 5, 9, 5, 5, 5, 5, 5, 5, 33, 44, 55}, new int[]{2, 5, 1, 4, 5, 2, 5});
+//        cemIndeksleriTap(new int[]{1, 5, 6, 3, 8, 2}, 9);
+//        sifirlariSagaYig(new int[]{1, 0, 4, 5, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0});
+//        tekEdedleriqaytar(new int[]{1, 2, 3, 11, 53, 4, 5, 6, 77, 7, 8, 9, 10});
+//        ikiArrayiSeteCevir(new int[]{1, 3, 1, 4, 6}, new int[]{3, 5, 1, 7, 8});
+//        enBoyukIkinciElement(new int[]{1, 4, 2, 6, 9, 5,7,8});
 
     }
 }
-
 
 
 
