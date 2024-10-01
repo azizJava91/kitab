596154c2b46ee2381db8230421ebb54efed78023
Aziz <niftalizadeaziz@gmail.com>
2024-10-01 09:22:16 +0400

    commit 3 :)

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import javax.swing.tree.TreeNode;
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Main {
 //    private static void artanPille() {
@@ -477,7 +474,214 @@ public class Main {
 //        }
 //        Arrays.stream(yekun).sorted().forEach(element -> System.out.print(element + ", "));
 //    }
+//    private static void enBoyukIkinciElement(int[] array) {
+////        {1, 4, 2, 6, 9, 5, 7, 8}
+//        int enboyuk = Integer.MIN_VALUE;
+//        int ikinciBoyuk = Integer.MIN_VALUE;
+//        for (int i = 0; i < array.length; i++) {
+//
+//            if (array[i] > enboyuk) {
+//                enboyuk = array[i];
+//            }
+//        }
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] < enboyuk && array[i] > ikinciBoyuk) {
+//                ikinciBoyuk = array[i];
+//            }
+//        }
+//
+//        if (ikinciBoyuk == Integer.MIN_VALUE) {
+//            System.out.println("Ikinci boyuk yoxdur");
+//        } else
+//            System.out.println("ikinci boyuk eded " + ikinciBoyuk);
+//    }
 
+    //    private static void arrayiTersineYaz(int[] array) {
+////        {1, 2, 3, 4, 5}
+//        int[] yekun = new int[array.length];
+//        int index = array.length;
+//
+//        for (int i = 0; i < array.length; i++) {
+//            yekun[--index] = array[i];
+//        }
+//        for (int i = 0; i < yekun.length; i++) {
+//            System.out.println(yekun[i]);
+//        }
+//    }
+//    private static void tekrarsizElementleriTap(int[] array) {
+////        {1,3,2,1,7,4,5,2,4}
+//
+//        int keciciCounter = 0;
+//        int[] kecici = new int[array.length];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            boolean tekrarsiz = true;
+//            for (int j = 0; j < array.length; j++) {
+//                if (i != j && array[i] == array[j]) {
+//                    tekrarsiz = false;
+//                    break;
+//                }
+//            }
+//            if (tekrarsiz) {
+//                kecici[keciciCounter++] = array[i];
+//            }
+//        }
+//
+//        int[] yekun = new int[keciciCounter];
+//
+//        for (int i = 0; i < yekun.length; i++) {
+//            yekun[i] = kecici[i];
+//        }
+//        if (yekun.length == 0) {
+//            System.out.println("tekrarsiz element yoxdur");
+//        }
+//        Arrays.stream(yekun).sorted().forEach(element-> System.out.println(element+","));
+//    }
+
+
+//    private static void hansiElementNeceDefe(int[] array) {
+////        {1,2,1,3,6,3,4,3,5,3}
+//        int[] SortedArray = Arrays.stream(array).sorted().toArray();
+//        int counter = 0;
+//        int index = 0;
+//
+//        for (int i = 0; i < SortedArray.length; i++) {
+//            boolean tekrarEmeliyyat = false;
+//            int hedef = SortedArray[i];
+//            for (int j = 0; j < SortedArray.length; j++) {
+//                if (j < index && hedef == SortedArray[j]) {
+//                    tekrarEmeliyyat = true;
+//                    break;
+//                }
+//                if (SortedArray[i] == SortedArray[j]) {
+//                    counter++;
+//                }
+//            }
+//            if (!tekrarEmeliyyat) {
+//                System.out.println(hedef + " reqemi: " + counter + " defe");
+//                counter = 0;
+//            }
+//            index++;
+//        }
+//    }
+
+    //    private static void ortalamadanBoyukElementler(double[] array) {
+////        {1,2,3,4,5}
+//        double toplam = 0;
+//
+//        for (int i = 0; i < array.length; i++) {
+//            toplam += array[i];
+//        }
+//
+//        double ortalama = toplam / array.length;
+//        int counter = 0;
+//        double[] ortalamadanBoyukler = new double[array.length];
+//
+//        for (int i = 0; i < ortalamadanBoyukler.length; i++) {
+//            if (array[i] > ortalama) {
+//                ortalamadanBoyukler[counter++] = array[i];
+//            }
+//        }
+//        double[] yekun = new double[counter];
+//
+//        for (int i = 0; i < yekun.length; i++) {
+//            yekun[i] = ortalamadanBoyukler[i];
+//        }
+//        if (counter == 0) System.err.println("ortalama = " + ortalama + "\n" + "ortalamadan boyuk element yoxdur");
+//        else {
+//            System.out.println("ortalama = " + ortalama);
+//            for (int i = 0; i < yekun.length; i++) {
+//                System.out.println("element " + (i + 1) + " ==> " + yekun[i]);
+//            }
+//        }
+//    }
+//    private static void customBubbleSort(int[] array) {
+//        int arrayUzunluq = array.length;
+//        boolean basdanSonaSiralidir;
+//
+//        for (int i = 0; i < arrayUzunluq - 1; i++) {
+//            basdanSonaSiralidir = true;
+//            for (int j = 0; j < arrayUzunluq - 1 - i; j++) {
+//                if (array[j] > array[j + 1]) {
+//                    int temp = array[j];
+//                    array[j] = array[j + 1];
+//                    array[j + 1] = temp;
+//                    basdanSonaSiralidir = false;
+//                }
+//            }
+//            if (basdanSonaSiralidir) break;
+//        }
+//        Arrays.stream(array).forEach(System.out::println);
+//    }
+
+    //    private static void ekobTap(int[] array) {
+//        int max = array[0];
+//
+//        for (int i = 1; i < array.length; i++) {
+//            if (array[i] > max) max = array[i];
+//        }
+//
+//        for (int i = max; i < Integer.MAX_VALUE; i++) {
+//            int counter = 0;
+//            for (int j = 0; j < array.length; j++) {
+//                if (i % array[j] == 0) {
+//                    counter++;
+//                }
+//            }
+//
+//            if (counter == array.length) {
+//                Arrays.stream(array).sorted().forEach(element -> System.err.print(element + ", "));
+//                System.out.println("edelerinin en kicik ortaq boluneni " + i + " dir");
+//                return;
+//            }
+//        }
+//    }
+
+    //   {3, 2, 11, 5, 9}
+//    private static void enBoyuk3Eded(int[] array) {
+//        int[] yekun = {Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE};
+//
+//        for (int eded : array) {
+//            if (eded > yekun[0]) {
+//                yekun[2] = yekun[1];
+//                yekun[1] = yekun[0];
+//                yekun[0] = eded;
+//            } else if (eded > yekun[1]) {
+//                yekun[2] = yekun[1];
+//                yekun[1] = eded;
+//            } else if (eded > yekun[2]) {
+//                yekun[2] = eded;
+//            }
+//        }
+//
+//        Arrays.stream(yekun).sorted().forEach(System.out::println);
+//    }
+//    private static void enBoyuk2Eded(int[] array) {
+//        int enBoyuk = Integer.MIN_VALUE;
+//        int ikinciBoyuk = Integer.MIN_VALUE;
+//
+////        {66, 1, 2, 3, 4, 5, 4,6, 66, 3, 2, 66}
+//        for (int j : array) {
+//            if (j > enBoyuk) {
+//                enBoyuk = j;
+//            }
+//
+//        }
+//        for (int j : array) {
+//            if (j > ikinciBoyuk && j <= enBoyuk) {
+//                ikinciBoyuk = j;
+//            }
+//
+//        }
+//
+//        if (enBoyuk == ikinciBoyuk) {
+//            System.err.println("Elementler eynidir");
+//        } else {
+//            System.out.println("en boyuk: " + enBoyuk);
+//            System.out.println("ikinci boyuk : " + ikinciBoyuk);
+//        }
+//    }
 
     public static void main(String[] args) {
 
@@ -503,8 +707,15 @@ public class Main {
 //        sifirlariSagaYig(new int[]{1, 0, 4, 5, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0});
 //        tekEdedleriqaytar(new int[]{1, 2, 3, 11, 53, 4, 5, 6, 77, 7, 8, 9, 10});
 //        ikiArrayiSeteCevir(new int[]{1, 3, 1, 4, 6}, new int[]{3, 5, 1, 7, 8});
-//        enBoyukIkinciElement(new int[]{1, 4, 2, 6, 9, 5,7,8});
-
+//        enBoyukIkinciElement(new int[]{9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9});
+//        arrayiTersineYaz(new int[]{1, 2, 3, 4, 5, 8, 3});
+//        tekrarsizElementleriTap(new int[]{14, 146, 14,4,3,7,86,7,6});
+//        hansiElementNeceDefe(new int[]{1, 2, 3, 4, 5, 4, 3, 2, 1});
+//        ortalamadanBoyukElementler(new double[]{3, 3, 3, 3, 3, 3});
+//        customBubbleSort(new int[]{2, 3, 4, 5, 9, 8});
+//        ekobTap(new int[]{4, 5, 10});
+//        enBoyuk3Eded(new int[]{14,3, 2, 11, 5, 9,12});
+//        enBoyuk2Eded(new int[]{6, 6, 6, 6, 6, 6});
     }
 }
 
